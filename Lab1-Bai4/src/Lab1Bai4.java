b99ba6bc2c65f090dca3b27ad7d252335cfd8ac5
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-02-11 16:10:24 +0700

    Lab1Bai4
    Đã xong bài 4 của lab1.

---
+++ Lab1-Bai4/src/Lab1Bai4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Lab1Bai4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a;
+		double b;
+		double c;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Nhập a: ");
+		a = scan.nextDouble();
+		System.out.print("Nhập b: ");
+		b = scan.nextDouble();
+		System.out.print("Nhập c: ");
+		c = scan.nextDouble();
+		
+		scan.close();
+		
+		double delta = Math.pow(b,2) - 4*a*c;
+		
+		System.out.printf("Delta = %f", delta);
+		System.out.println();
+		System.out.printf("Căn bậc 2 của Delta = %f", Math.sqrt(delta));
+	}
+
+}
