17a9e69803a19b35e54a9eb30ea0e7e0a7584591
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-02-11 15:49:55 +0700

    Lab1Bai2
    Đã xong bài 2 của lab1

---
+++ Lab1-Bai2/src/Lab1Bai2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Lab1Bai2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double chieuDai;
+		double chieuRong;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.printf("Nhập chiều dài: ");
+		chieuDai = scan.nextDouble();
+		System.out.printf("Nhập chiều rộng: ");
+		chieuRong = scan.nextDouble();
+		scan.close();
+		
+		System.out.printf("Chu vi hình chữ nhật: %f", (chieuDai + chieuRong)*2);
+		System.out.println();
+		System.out.printf("Diện tích hình chữ nhật: %f", (chieuDai * chieuRong));
+		System.out.println();
+		System.out.printf("Chiều dài của cạnh nhỏ nhất: %f", Math.min(chieuDai, chieuRong));
+	}
+}
