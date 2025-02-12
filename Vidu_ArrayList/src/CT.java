e0b54a443d14d6f4068e5c17218fd426943db642
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-02-12 20:43:04 +0700

    update Vidu_ArrayList
    Cập nhật lại file CT.java

--- Vidu_ArrayList/src/CT.java
+++ Vidu_ArrayList/src/CT.java
@@ -9,12 +9,12 @@ public class CT {
 		dsSanPham = new ArrayList<SanPham>();
 		
 		// Nhập 3 sản phẩm	
-		SanPham sp1 = new SanPham(0, "Kem", "Gỉai khát", null);
+//		SanPham sp1 = new SanPham(0, "Kem", "Gỉai khát", null);
 		SanPham sp2 = new SanPham(1, "Cơm", "Đồ ăn", null);
 		SanPham sp3 = new SanPham(2, "Phở", "Đồ ăn", null);
 	
 		// Thêm vào ArrayList
-		dsSanPham.add(sp1);
+		dsSanPham.add(new SanPham(0, "Kem", "Gỉai khát", null));
 		dsSanPham.add(sp2);
 		dsSanPham.add(sp3);
 		
