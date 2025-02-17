1cc3366963f13cfe5a83847ea19c59f65e1b712e
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-02-17 19:47:09 +0700

    Update Lab2Bai3
    Cập nhật code

--- Lab2-Bai3-TinhTienDien/src/Lab2Bai3.java
+++ Lab2-Bai3-TinhTienDien/src/Lab2Bai3.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class Lab2Bai3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double soDien;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Nhập số điện (kw): ");;
+		soDien = scan.nextDouble();
+		scan.close();
+		
+		if(soDien < 50)
+			System.out.printf("Số tiền điện: %f vnđ", soDien*1000);
+		else 
+			System.out.printf("Số tiền điện: %f vnđ", 50*1000 + (soDien - 50)*1200);
 
 	}
-
 }
