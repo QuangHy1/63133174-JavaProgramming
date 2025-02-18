4ff8fdd5eff5c899bd3b022e118ce5bf1c9e68a5
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-02-18 10:10:39 +0700

    Lab3-Bai2
    Đã xong

--- Lab3-Bai2-BangCuuChuong/src/Lab3Bai2.java
+++ Lab3-Bai2-BangCuuChuong/src/Lab3Bai2.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
 
 public class Lab3Bai2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Bảng cửu chương: ");
+		a = scan.nextInt();
+		scan.close();
+		for(int i=1; i<=10; i++) {
+			System.out.printf("%d x %d = %d\n", a, i, a*i);
+		}
+		
+		
 	}
 
 }
