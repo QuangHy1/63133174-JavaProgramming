42cb39abc6fd84a43ae1fa4bfb187cd834423a26
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-02-19 07:51:06 +0700

    Lab4-Bai1
    Đã xong và sửa Lab5-Bai1

--- Lab4-Bai1-ThueNhapKhau/src/Lab4Bai1.java
+++ Lab4-Bai1-ThueNhapKhau/src/Lab4Bai1.java
@@ -1,23 +1,55 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Lab4Bai1 {
-	public static void main(String[] args) {
-		ArrayList<Double> list = new ArrayList<>();
+	public String tenSP;
+	public double donGia;
+	public double giamGia;
+	
+	public Lab4Bai1(String tenSP, double donGia, double giamGia) {
+	    this.tenSP = tenSP;
+	    this.donGia = donGia;
+	    this.giamGia = giamGia;
+	}
+
+
+	public void nhap() {
 		Scanner scan = new Scanner(System.in);
-		double a;
-		System.out.println("Nhập một số vào, ấn 0 để dừng:");
-		do {
-			a = scan.nextDouble();
-			if(a != 0) {
-				list.add(a);
-			}
-		}while(a != 0);
 		
-		System.out.println("\n Danh sách các số vừa nhập: ");
-		for (Double number : list) {
-            System.out.println(number);
-        }
+		System.out.print("Nhập tên sản phẩm: ");
+		tenSP = scan.nextLine();
+		System.out.print("Nhập giá sản phẩm: ");
+		donGia = scan.nextDouble();
+		System.out.print("Nhập giảm giá: ");
+		giamGia = scan.nextDouble();
+	}
+	
+	public double getThueNhapKhau(double donGia) {
+		return donGia * 0.1;
+	}
+	
+	public void Xuat() {
+		System.out.printf("Tên sản phẩm: %s\t Đơn giá: %f\t Giảm giá: %f\t Thuế nhập khẩu: %f\n", tenSP, donGia, giamGia, getThueNhapKhau(donGia));
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n;
+		System.out.print("Nhập số lượng sản phẩm: ");
+		n = scan.nextInt();
+		
+		Lab4Bai1[] SPList = new Lab4Bai1[n];
+		for(int i=0; i<n; i++) {
+			SPList[i] = new Lab4Bai1("", 0, 0);
+			System.out.printf("\nSản phẩm thứ %d:\n", i+1);
+			SPList[i].nhap();
+		}
+		System.out.println("Thông tin sản phẩm vừa nhập: ");
+		for(int i=0; i<n; i++) {
+			SPList[i].Xuat();
+		}
+		
 		scan.close();
 	}
 }
--- Lab5-Bai1-ArrayList1/src/Lab5Bai1.java
+++ Lab5-Bai1-ArrayList1/src/Lab5Bai1.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Lab5Bai1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<Double> list = new ArrayList<>();
+		Scanner scan = new Scanner(System.in);
+		double a;
+		System.out.println("Nhập một số vào, ấn 0 để dừng:");
+		do {
+			a = scan.nextDouble();
+			if(a != 0) {
+				list.add(a);
+			}
+		}while(a != 0);
+		
+		System.out.println("\n Danh sách các số vừa nhập: ");
+		for (Double number : list) {
+            System.out.println(number);
+        }
+		scan.close();
 	}
 
 }
