ab5018718a0569a9be9c941ed0987d3d6234b044
QuangHy1 <150933991+QuangHy1@users.noreply.github.com>
2025-02-19 07:41:03 +0700

    Lab5-Bai1
    Đã xong

--- Lab4-Bai1-ThueNhapKhau/src/Lab4Bai1.java
+++ Lab4-Bai1-ThueNhapKhau/src/Lab4Bai1.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Lab4Bai1 {
-
+	public static void main(String[] args) {
+		ArrayList<Double> list = new ArrayList<>();
+		Scanner scan = new Scanner(System.in);
+		double a;
+		System.out.println("Nhập một số vào, ấn 0 để dừng:");
+		do {
+			a = scan.nextDouble();
+			if(a != 0) {
+				list.add(a);
+			}
+		}while(a != 0);
+		
+		System.out.println("\n Danh sách các số vừa nhập: ");
+		for (Double number : list) {
+            System.out.println(number);
+        }
+		scan.close();
+	}
 }
